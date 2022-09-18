4eec8234061f88691ff4c7acd58876fc25b8aa48
Gryffon Bellish <owenbellish@gmail.com>
2022-09-18 14:36:17 -0400

    reorder things

--- src/params.rs
+++ src/params.rs
@@ -1,6 +1,12 @@
 use std::sync::Arc;
 
-use axum::{extract::Query, response::IntoResponse, Extension};
+use axum::{
+	extract::{ws::Message, Query},
+	http::StatusCode,
+	response::IntoResponse,
+	Extension,
+};
+use futures_util::SinkExt;
 use serde::Deserialize;
 
 use crate::state::State;
@@ -15,5 +21,32 @@ pub async fn handler(
 	Query(params): Query<RedirectParams>,
 	Extension(state): Extension<Arc<State>>,
 ) -> impl IntoResponse {
-	state.handle_redirect(params).await
+	handle_redirect(state, params).await
+}
+
+async fn handle_redirect(
+	state: Arc<State>,
+	RedirectParams {
+		state: state_value,
+		code,
+	}: RedirectParams,
+) -> impl IntoResponse {
+	match state.0.write().await.remove(state_value.as_str()) {
+		None => {
+			log::warn!("state parameter not sent via ws before redirect");
+			return (StatusCode::BAD_REQUEST, "state parameter not registered");
+		}
+		Some(mut rx) => {
+			log::info!("code {} successfully sent for state {}", code, state_value);
+			let data = serde_json::json!({
+				"code": code,
+				"state": state_value
+			});
+			let _ = rx
+				.send(Message::Text(serde_json::to_string(&data).unwrap()))
+				.await;
+
+			return (StatusCode::OK, "You may now close this tab");
+		}
+	}
 }
